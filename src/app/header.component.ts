0e8f3174245a435b1820405ca32ff0a190248693
Chris Parker <chris@parkerdesignbuild.com>
2024-06-12 20:40:37 -0400

    set up

---
+++ src/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: 'header.component.html',
+  imports: [
+    NgOptimizedImage
+  ],
+  styleUrl: 'header.component.css'
+})
+
+export class HeaderComponent {}
